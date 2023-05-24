beebe2e5b6706fd32523278881c229752354362c
shanghaik168@gmail.com <Shanghaik1998>
2023-05-24 08:27:57 +0700

    code

---
+++ AppViews/Controllers/MausacController.cs
@@ -0,0 +1,25 @@
+﻿using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AppViews.Controllers
+{
+    public class MausacController : Controller
+    {
+        public MausacController() { 
+        }
+        public async Task<IActionResult> ShowAllColor()
+        {
+            string apiURL = 
+                "https://localhost:7179/api/Mausac/get-all-color";
+            // Sau khi có URL thì thực hiện việc lấy dữ liệu trả về từ nó
+            var httpClient = new HttpClient(); // Tại 1 httpClient để call API
+            var response = await httpClient.GetAsync(apiURL); // Lấy kết quả
+                                                              // Đọc ra string Json
+            string apiData = await response.Content.ReadAsStringAsync();
+            // Lấy kết quả thu được bằng cách bóc dữ liệu Json
+            var result = JsonConvert.DeserializeObject<List<MauSac>>(apiData);
+            return View(result);
+        }
+    }
+}
--- TestAPICS/Program.cs
+++ TestAPICS/Program.cs
@@ -1,32 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Newtonsoft.Json;
 
-//Console.WriteLine("Enter FE marks");
-//double fe = Convert.ToDouble(Console.ReadLine());
-//Console.WriteLine("Enter BE marks");
-//double be = Convert.ToDouble(Console.ReadLine());
-//Console.WriteLine("Enter Algo marks");
-//double alg = Convert.ToDouble(Console.ReadLine());
-//// Gán dữ liệu mình vừa lấy được vào API URL
-////https://localhost:7011/WeatherForecast/evaluate-study?fe=1&be=1&alg=1
-//string apiURL = $"https://localhost:7011/WeatherForecast/" +
-//    $"evaluate-study?fe={fe}&be={be}&alg={alg}";
-//// Sau khi có URL thì thực hiện việc lấy dữ liệu trả về từ nó
-//var httpClient = new HttpClient(); // Tại 1 httpClient để call API
-//var response = await httpClient.GetAsync(apiURL); // Lấy kết quả
-//                                                  // Đọc ra string Json
-//string apiData = await response.Content.ReadAsStringAsync();
-//// Lấy kết quả thu được bằng cách bóc dữ liệu Json
-//string result = JsonConvert.DeserializeObject<string>(apiData);
-//string show = "";
-//// Logic code 
-//double avg = Convert.ToDouble(result);
-//if (avg < 5) show = "Học lại";
-//else if (avg < 6.5) show = "Quite good";
-//else if (avg < 8) show = "Excellent";
-//else if (avg < 9.5) show = "Good";
-//else show = "Perfect";
-// Console.WriteLine(show);
+Console.WriteLine("Enter FE marks");
+double fe = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter BE marks");
+double be = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Algo marks");
+double alg = Convert.ToDouble(Console.ReadLine());
+// Gán dữ liệu mình vừa lấy được vào API URL
+//https://localhost:7011/WeatherForecast/evaluate-study?fe=1&be=1&alg=1
+string apiURL = $"https://localhost:7011/WeatherForecast/" +
+    $"evaluate-study?fe={fe}&be={be}&alg={alg}";
+// Sau khi có URL thì thực hiện việc lấy dữ liệu trả về từ nó
+var httpClient = new HttpClient(); // Tại 1 httpClient để call API
+var response = await httpClient.GetAsync(apiURL); // Lấy kết quả
+                                                  // Đọc ra string Json
+string apiData = await response.Content.ReadAsStringAsync();
+// Lấy kết quả thu được bằng cách bóc dữ liệu Json
+string result = JsonConvert.DeserializeObject<string>(apiData);
+string show = "";
+// Logic code 
+double avg = Convert.ToDouble(result);
+if (avg < 5) show = "Học lại";
+else if (avg < 6.5) show = "Quite good";
+else if (avg < 8) show = "Excellent";
+else if (avg < 9.5) show = "Good";
+else show = "Perfect";
+Console.WriteLine(show);
 A a = new A();
 a.Id = 1;
 A aa = a;
